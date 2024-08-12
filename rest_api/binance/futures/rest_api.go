d277f0cec6ae29fd6acab27420df0fd9157752b0
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2024-08-12 12:17:23 +0300

    refactor: Update Binance futures REST API endpoints
    Update the REST API endpoints for Binance futures in the `rest_api/binance/futures/rest_api.go` file. The endpoints were changed from `/api/v1/listenKey` to `/fapi/v1/listenKey` in order to align with the Binance API documentation.
    
    Closes #456

--- rest_api/binance/futures/rest_api.go
+++ rest_api/binance/futures/rest_api.go
@@ -50,7 +50,7 @@ func (ra *RestApi) QueryAllOrders() *request.Request {
 }
 
 func (ra *RestApi) ListenKey() (listenKey string, err error) {
-	response, err := rest_api.CallRestAPI(ra.apiBaseUrl, http.MethodPost, nil, "/api/v1/listenKey", ra.sign)
+	response, err := rest_api.CallRestAPI(ra.apiBaseUrl, http.MethodPost, nil, "/fapi/v1/listenKey", ra.sign)
 	if err != nil {
 		err = fmt.Errorf("error calling API: %v", err)
 		return
@@ -60,12 +60,12 @@ func (ra *RestApi) ListenKey() (listenKey string, err error) {
 }
 
 func (ra *RestApi) KeepAliveListenKey() (err error) {
-	_, err = rest_api.CallRestAPI(ra.apiBaseUrl, http.MethodPut, nil, "/api/v1/listenKey", ra.sign)
+	_, err = rest_api.CallRestAPI(ra.apiBaseUrl, http.MethodPut, nil, "/fapi/v1/listenKey", ra.sign)
 	return
 }
 
 func (ra *RestApi) CloseListenKey() (err error) {
-	_, err = rest_api.CallRestAPI(ra.apiBaseUrl, http.MethodDelete, nil, "/api/v1/listenKey", ra.sign)
+	_, err = rest_api.CallRestAPI(ra.apiBaseUrl, http.MethodDelete, nil, "/fapi/v1/listenKey", ra.sign)
 	return
 }
 
