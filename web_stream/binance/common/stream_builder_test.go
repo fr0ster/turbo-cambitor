fc4cb911da1e1368da86228205d9cb899f224f7b
Oleksii Kyslytsia <oleksij.kyslytsja@gmail.com>
2025-08-23 14:45:53 +0400

    feat: add support for custom dialer and socket factory in StreamBuilder

--- web_stream/binance/common/stream_builder_test.go
+++ web_stream/binance/common/stream_builder_test.go
@@ -1,13 +1,19 @@
 package common_web_stream_test
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
+
+	"sync/atomic"
 
 	common "github.com/fr0ster/turbo-cambitor/common"
 	common_web_stream "github.com/fr0ster/turbo-cambitor/web_stream/binance/common"
+	web_socket "github.com/fr0ster/turbo-restler/web_socket"
 	"github.com/gorilla/websocket"
 )
 
@@ -58,5 +64,74 @@ func TestStreamBuilder_AggTrades(t *testing.T) {
 	_, err := stream.Stream().Connect()
 	if err != nil {
 		t.Fatalf("failed to connect to mock websocket: %v", err)
+	}
+}
+
+func TestStreamBuilder_WithDialer(t *testing.T) {
+	// Mock WS server
+	server := httptest.NewServer(http.HandlerFunc(mockWSHandler))
+	defer server.Close()
+
+	hostOnly := strings.TrimPrefix(server.URL, "http://")
+	symbol := "btcusdt"
+
+	// Custom dialer that toggles a flag via NetDialContext
+	var used atomic.Bool
+	d := *websocket.DefaultDialer
+	d.NetDialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		used.Store(true)
+		var nd net.Dialer
+		return nd.DialContext(ctx, network, addr)
+	}
+
+	builder := common_web_stream.NewStreamBuilder(
+		common.WsScheme("ws"),
+		common.WsHost(hostOnly+"/ws"),
+		common.WsEndpoint(""),
+		symbol,
+	).WithDialer(&d)
+
+	// Build and connect
+	_, err := builder.AggTrades().Stream().Connect()
+	if err != nil {
+		t.Fatalf("failed to connect with custom dialer: %v", err)
+	}
+	if !used.Load() {
+		t.Fatalf("expected custom dialer to be used, but it wasn't")
+	}
+}
+
+func TestStreamBuilder_WithFactory(t *testing.T) {
+	// Mock WS server
+	server := httptest.NewServer(http.HandlerFunc(mockWSHandler))
+	defer server.Close()
+
+	hostOnly := strings.TrimPrefix(server.URL, "http://")
+	symbol := "btcusdt"
+
+	var factoryUsed atomic.Bool
+	factory := func(url string) (web_socket.WebSocketCommonInterface, error) {
+		factoryUsed.Store(true)
+		return web_socket.NewWebSocketWrapper(websocket.DefaultDialer, url)
+	}
+
+	builder := common_web_stream.NewStreamBuilder(
+		common.WsScheme("ws"),
+		common.WsHost(hostOnly+"/ws"),
+		common.WsEndpoint(""),
+		symbol,
+	).WithFactory(factory)
+
+	// Ensure that if both factory and dialer are set, factory wins
+	d := *websocket.DefaultDialer
+	d.HandshakeTimeout = 2 * time.Second
+	builder = builder.WithDialer(&d)
+
+	_, err := builder.AggTrades().Stream().Connect()
+	if err != nil {
+		t.Fatalf("failed to connect with custom factory: %v", err)
+	}
+	if !factoryUsed.Load() {
+		t.Fatalf("expected custom factory to be used, but it wasn't")
 	}
 }
